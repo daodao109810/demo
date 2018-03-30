259b34df050510775cdc904818786ce3581cabf9
76246 <762463276@qq.com>
2018-03-30 10:42:03 +0800

    历史

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -2,5 +2,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("我吃鸡腿");
         System.out.println("鸡腿是傻逼");
+        System.out.println("回滚到历史版本");
     }
 }
